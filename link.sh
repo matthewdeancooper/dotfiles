8cbe060b29014f385ed86df087cb4352ee0e3bc6
matthew <mcoo@protonmail.com>
2019-07-17 15:16:47 +1000

    dot

--- link.sh
+++ link.sh
@@ -1,12 +1,14 @@
+## home
 ln -s ~/dotfiles/tmuxconf ~/.tmux.conf
 ln -s ~/dotfiles/.xinitrc ~/.xinitrc
 ln -s ~/dotfiles/.zshrc ~/.zshrc
 ln -s ~/dotfiles/.zprofile ~/.zprofile
-ln -s ~/dotfiles/init.vim ~/.config/nvim/init.vim
-ln -s ~/dotfiles/userdirs-dirs ~/.config/user-dirs.dirs
-ln -s ~/dotfiles/zathurarc ~/.config/zathura/zathurarc
-ln -s ~/dotfiles/mutttheme ~/.config/mutt/mutttheme
 ln -s ~/dotfiles/.spacemacs ~/.spacemacs
 ln -s ~/dotfiles/.offlineimap.py ~/.offlineimap.py
 ln -s ~/dotfiles/.offlineimaprc ~/.offlineimaprc
+## .config
+ln -s ~/dotfiles/init.vim ~/.config/nvim/init.vim
+ln -s ~/dotfiles/userdirs-dirs.dirs ~/.config/user-dirs.dirs
+ln -s ~/dotfiles/zathurarc ~/.config/zathura/zathurarc
+ln -s ~/dotfiles/mutttheme ~/.config/mutt/mutttheme
 ln -s ~/dotfiles/dunstrc ~/.config/dunst/dunstrc
