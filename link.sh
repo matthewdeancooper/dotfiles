7eb5c4c8eb1577aa4007b69779f9fc631881ddd2
Matthew <matthewdeancooper@gmail.com>
2021-01-18 14:33:54 +1100

    update

--- link.sh
+++ link.sh
@@ -5,22 +5,12 @@ ln -sf $DIR/.spacemacs ~/.spacemacs
 ln -sf $DIR/.bashrc ~/.bashrc
 ln -sf $DIR/.gitconfig ~/.gitconfig
 ln -sf $DIR/.selected_editor ~/.selected_editor
+ln -sf $DIR/.tmuxconf ~/.tmux.conf
 
 # My extensions in home
 ln -sf $DIR/.profile_exports ~/.profile_exports
 ln -sf $DIR/.bash_aliases ~/.bash_aliases
 
-# ln -sf ~/github/dotfiles/.tmuxconf ~/.tmux.conf
-# ln -sf ~/proj/etc/.xinitrc ~/.xinitrc
-#ln -s ~/github/dotfiles/.zshrc ~/.zshrc
-# ln -s ~/proj/etc/.offlineimap.py ~/.offlineimap.py
-# ln -s ~/proj/etc/.offlineimaprc ~/.offlineimaprc
-# ln -sf ~/github/dotfiles/.fehbg ~/.fehbg
-
-
 ## .config
-# ln -sf ~/github/dotfiles/init.vim ~/.config/nvim/init.vim
-# ln -s ~/proj/etc/zathurarc ~/.config/zathura/zathurarc
-# ln -s ~/proj/etc/dunstrc ~/.config/dunst/dunstrc
-# ln -sf ~/github/dotfiles/user-dirs.dirs ~/.config/user-dirs.dirs
+ln -sf $DIR/init.vim ~/.config/nvim/init.vim
 ln -sf $DIR/user-dirs.dirs ~/.config/user-dirs.dirs
