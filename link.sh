e17f8bdcd99b1ba16e2f1dfa5ab80e81859fb84b
Matthew Cooper <matthewdeancooper@gmail.com>
2021-02-25 09:47:54 +1100

    updated

--- link.sh
+++ link.sh
@@ -7,10 +7,12 @@ ln -sf $DIR/.gitconfig ~/.gitconfig
 ln -sf $DIR/.selected_editor ~/.selected_editor
 ln -sf $DIR/.tmuxconf ~/.tmux.conf
 
-# My extensions in home
+# Extensions in home
 ln -sf $DIR/.profile_exports ~/.profile_exports
 ln -sf $DIR/.bash_aliases ~/.bash_aliases
 
 ## .config
 ln -sf $DIR/init.vim ~/.config/nvim/init.vim
 ln -sf $DIR/user-dirs.dirs ~/.config/user-dirs.dirs
+ln -sf $DIR/gtk.css ~/.config/gtk-3.0/gtk.css
+ln -sf $DIR/htoprc ~/.config/htop/htoprc
